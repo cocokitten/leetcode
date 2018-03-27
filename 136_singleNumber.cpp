e09072250474afc0e21c799e8a9325341e3416f2
CocoKitten <yangli2309@163.com>
2018-03-27 13:45:34 +0800

    add 136 single number

---
+++ 136_singleNumber.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+
+//Given an array of integers, every element appears twice except for one. Find that single one.
+
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+
+		map<int,int> nmap;
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (nmap.find(nums[i]) == nmap.end()) {
+				nmap[nums[i]] = 1;
+			}
+			else nmap[nums[i]]++;
+		}
+
+		map<int, int>::iterator it = nmap.begin();
+
+		while (it!=nmap.end())
+		{
+			if (it->second == 1)
+				return it->first;
+			else
+				it++;
+		}
+	}
+};
+
+
+int main() {
+
+
+	vector<int>nums = { 2,3,1,1,4,3,2,6,1,4 };
+	map<int, int> nmap;
+
+	Solution sol;
+	cout<<sol.singleNumber(nums)<<endl;
+
+	system("pause");
+	return 0;
+
+}

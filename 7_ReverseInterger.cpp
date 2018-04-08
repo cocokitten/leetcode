3233ab9afdcb72382606c5d5af91c9af9ddb90c3
Li Yang <liyang@LideMacBook-Pro.local>
2018-04-08 20:16:46 +0800

    update

--- 7_ReverseInterger.cpp
+++ 7_ReverseInterger.cpp
@@ -18,7 +18,7 @@ the 32 - bit signed integer range.For the purpose of this problem, assume that y
 function returns 0 when the reversed integer overflows.
 */
 
-
+//题目本身非常简单，但是需要考虑反转后溢出的情况
 #include<iostream>
 using namespace std;
 
@@ -26,10 +26,8 @@ class solution {
 
 public:
 	int reverse(int x) {   
-
 		bool flag = false;
 		if (x < 0) { x = -x; flag = true; }
-
 		int result = 0;
 		int remainder;
 		while (x != 0)
@@ -42,4 +40,19 @@ public:
 		if (flag) return -result;
 		return result;
 	}
+	
+	//使用long
+	int reverse2(int x){
+
+		int remainder = 0;
+		long result = 0;
+		while(x!=0){
+			remainder = x % 10;
+			result = result * 10 + remainder;
+			if(result > INT_MAX ||result < INT_MIN ) return 0;
+			x /= 10;
+		}
+		return result;
+	}
+
 };
